6626512f905160d27e3d69b56a997bc2ed54ef6b
yossefbanhaim <73604318+yossefbenhaim@users.noreply.github.com>
2023-02-21 19:05:34 +0200

    fix(colorPaletteStyle.ts): fixes it so that it wouldn't go up the title when scrolling .

--- src/components/colorPalette/colorPaletteStyle.ts
+++ src/components/colorPalette/colorPaletteStyle.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
     button: {
         height: '39px',
-        zIndex: '0',
+        zIndex: '1',
 
         '& .MuiButtonBase-root': {
             minHeight: '39px',
